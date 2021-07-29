75530a950bfd134a46d59f0dc73421fb79fa1ec5
youngmoon525 <77032646+youngmoon525@users.noreply.github.com>
2021-07-29 10:19:02 +0900

    2021-07-29:1
    1교시

--- WorkSpace/12.Collection/src/pack05_Japangi/DrinkDAO.java
+++ WorkSpace/12.Collection/src/pack05_Japangi/DrinkDAO.java
@@ -1,6 +1,7 @@
 package pack05_Japangi;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DrinkDAO {
 	// Method의 이름은 소문자로 시작 , Class의 이름은 대문자로 시작
@@ -17,4 +18,55 @@ public class DrinkDAO {
 			System.out.println(list.get(i).getName() + ".(" + list.get(i).getPrice() + ")" + list.get(i).getCompany());
 		}
 	}
+	
+	//사용자가 어떤값을 입력했을때 int또는 String형식으로 다시 값을 반환받기 위한
+	//메소드 
+	//ex)음료의 번호를 사용자가 선택함 -int
+	//ex)사용자가 금액을 입력함 -int 
+	//ex)rtn String 문자열의 길이가 적어도 1글자 이상은 들어오게끔 처리를 ..
+	//String을 리턴하는 메소드
+	//void == return이 없는 경우 
+	//그외 == return이 있는 경우
+	//void != String == return을 줘야함
+//	public static void main(String[] args) {
+//		//클래스의 메소드 기능 확인 
+//		DrinkDAO dao = new DrinkDAO();
+//		//System.out.println(dao.rtnString() ) ;
+//		System.out.println(dao.rtnInt());
+//	} //test용 main 메소드 개발완료후 삭제 요청 
+	
+	public String rtnString() {
+		//반복해서 옳은 값이 나올때까지 입력을 유도해야함
+		Scanner sc = new Scanner(System.in);
+		String rtnStr = "";
+		while (true) {
+			System.out.println("적어도 한글자 이상의 문자를 입력해주세요.");
+			rtnStr = sc.nextLine();
+			//trim으로 공백문자를 제거 
+			if (rtnStr.trim().length() > 0) {
+				return rtnStr;
+			}
+		}//무한루프 되는 반복문 밑에 코드는 실행 될 수가 없기 때문에 오류 코드라고 인식함
+		 //메소드 구조만 먼저 만듬
+	}//method
+	
+	//int형을 리턴하는 메소드 
+	public int rtnInt() {
+		Scanner sc = new Scanner(System.in);
+		int rtni =0; 
+		while (true) {
+			//try Catch
+			try {
+				System.out.println("숫자를 입력해주세요.");
+				 rtni = Integer.parseInt(sc.nextLine()); ///pass nextint()
+				 return rtni; 
+				//에러가 발생 안하면 int 
+				//발생 하면 String 
+			} catch (Exception e) {
+		
+			}
+			
+		}
+	}
+	
 }
--- WorkSpace/12.Collection/src/pack05_Japangi/MainProgram.java
+++ WorkSpace/12.Collection/src/pack05_Japangi/MainProgram.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainProgram {
+	public static int coin ;
 	public static void main(String[] args) {
 		//실제 자판기의 프로그램이 실행되서 동작하는 부분
 		System.out.println("자판기 프로그램이 시작됨");
 		System.out.println("1.관리자 모드  2.사용자 모드 ▶ 그 외 키를 누르면 종료 됩니다.");
-		Scanner sc = new Scanner(System.in);
-		String inputData = sc.nextLine();
+		//Scanner sc = new Scanner(System.in);
+		//String inputData = sc.nextLine(); 
+		//▶==스캐너로 입력받는 부분들은 DAO의 메소드로 처리
+		
 		//Drink <음료라는 객체는 String , int , String ..등등의 속성을 여러가지 변수타입으로
 		//가지고 있기때문에 DTO라는 속성의 묶음(객체)를 만들어야 된다.
 		//ArrayList<String> name = new ArrayList<String>();
@@ -28,6 +31,7 @@ public class MainProgram {
 		//인스턴스 멤버의 사용 
 		DrinkDAO dao = new DrinkDAO();
 		dao.display(list);
+		String inputData = dao.rtnString();
 		if (inputData.equals("1")) {
 			System.out.println("관리자 모드 ");
 			//음료의 추가 수정 삭제 조회
@@ -39,18 +43,18 @@ public class MainProgram {
 			
 			//1.데이터의 추가(음료추가) 
 			System.out.println("1.음료 추가 2.음료 삭제 3. 음료 수정 4.음료 다시보기");
-			inputData = sc.nextLine();
+			inputData = dao.rtnString();
 			if(inputData.equals("1")) {
 				//음료 추가 로직을 구현 
 				System.out.println("음료 추가 로직 시작");
 				DrinkDTO dto = new DrinkDTO();
 				//dto.name , price , compay = null , 0 , null
 				System.out.println("음료의 이름을 입력해주세요");
-				dto.setName( sc.nextLine() ) ;
+				dto.setName( dao.rtnString() ) ;
 				System.out.println("음료의 가격을 입력해주세요");
-				dto.setPrice( Integer.parseInt( sc.nextLine() )) ;
+				dto.setPrice( dao.rtnInt() ) ;
 				System.out.println("음료의 회사를 입력해주세요");
-				dto.setCompany( sc.nextLine() ) ;
+				dto.setCompany( dao.rtnString() ) ;
 				list.add(dto);
 				dao.display(list);
 			}else if(inputData.equals("2")) {
@@ -58,22 +62,33 @@ public class MainProgram {
 				System.out.println("음료 삭제 시작");
 				dao.display(list);//음료의 목록을 다시보여주고 해당하는 번호를 선택하게끔 유도
 				System.out.println("삭제할 번호를 선택 하세요");
-				inputData = sc.nextLine();
-				list.remove(Integer.parseInt( inputData )-1 ) ;//음료 삭제 완료
+			
+				list.remove( dao.rtnInt()-1 ) ;//음료 삭제 완료
 				dao.display(list);
 			}else if(inputData.equals("3")){
 				//음료 수정 로직을 구현
 				System.out.println("음료 수정 시작");
 				System.out.println("수정할 번호를 선택 하세요");
-				inputData = sc.nextLine();
+				//inputData = sc.nextLine();
 				
 				System.out.println("음료의 이름을 입력해주세요.");
-				list.get(Integer.parseInt(inputData)-1 ).setName(sc.nextLine());
+				
+				list.get(dao.rtnInt()-1 ).setName(dao.rtnString() );
 				dao.display(list);
 				//..반복 처리 
 			}
 		}else if(inputData.equals("2")) {
 			System.out.println("사용자 모드");
+			dao.display(list);
+			System.out.println("금액을 입력해주세요.");
+			coin = dao.rtnInt();
+			
+			System.out.println("음료를 선택해 주세요.");
+	
+			coin = coin - list.get(dao.rtnInt()-1).getPrice();
+			System.out.println("잔돈"+ coin);
+			
+			//반복되는 코드를 찾는다. => 메소드로 나누고
 		}else {
 			System.out.println("프로그램을 종료 합니다.");
 		}
